f5fb1b9787db6b232558bcc0a89bf78fb4732cbc
GoktugOcalan95 <goktug.ocalan.95@gmail.com>
2022-05-26 02:25:36 -0400

    Agenda scheduler skip immediate run

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -21,5 +21,5 @@ export async function setupAgenda(mongoDb: Db): Promise<void> {
 
   await agenda.start();
   const agendaScheduleTimeString = dailyUpdateTimeString || defaultDailyUpdateTimeString;
-  await agenda.every(agendaScheduleTimeString, "update dataset");
+  await agenda.every(agendaScheduleTimeString, "update dataset", null, { skipImmediate: true });
 }
