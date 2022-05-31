71806a43219c71efbdf9891f078a8404e99977b6
GoktugOcalan95 <goktug.ocalan.95@gmail.com>
2022-05-31 06:01:36 -0400

    Added promise rejection handling

--- src/components/userRating/routes.ts
+++ src/components/userRating/routes.ts
@@ -5,37 +5,21 @@ import { UserRatingController } from "./controller";
 const router = Router();
 
 router.get("/parse", extractJWT, (_req, res) => {
-
   // eslint-disable-next-line
-  if (!res?.locals?.jwt?.userId) {
-    return res.status(400).json({
-      message: "Invalid token",
-    });
-  }
-
-  // eslint-disable-next-line
-  void UserRatingController.parseUserRatings(res.locals.jwt.userId);
+  void UserRatingController.parseUserRatings(res?.locals?.jwt?.userId);
   res.sendStatus(200);
 });
 
 // GET Rating By Query
 router.get("/", extractJWT, (async (req, res) => {
-
   // eslint-disable-next-line
-  if (!res?.locals?.jwt?.userId) {
-    return res.status(400).json({
-      message: "Invalid token",
+  await UserRatingController.getByUserId(res?.locals?.jwt?.userId, req.query)
+    .then(titles => res.send(titles))
+    .catch(() => {
+      return res.status(500).json({
+        message: "An unexpected error occured",
+      });
     });
-  }
-
-  // eslint-disable-next-line
-  const titles = await UserRatingController.getByUserId(res.locals.jwt.userId, req.query);
-  if (!titles) {
-    return res.status(404).json({
-      message: "Results not found",
-    });
-  }
-  res.send(titles);
 }) as RequestHandler);
 
 export const userRatingRoutes = router;
